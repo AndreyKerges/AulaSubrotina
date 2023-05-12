c74e160cd287fffed7fd6e1b9d4ad058482bad60
andrey.nascimento4@etec.sp.gov.br <e107ti@etec.sp.gov.br>
2023-05-12 20:28:57 -0300

    exercicio finalizado

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿//DRY - Don´t Repeat Yourself
+
+ExibirDivisor();
+
+Console.WriteLine("a");
+
+ExibirDivisor();
+
+Console.WriteLine("b");
+
+ExibirDivisor();
+
+Console.WriteLine("c");
+
+ExibirDivisor();
+
+Console.WriteLine("d");
+
+ExibirDivisor();
+
+Console.WriteLine("e");
+
+ExibirDivisor();
+
+void ExibirDivisor()
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("--------------------------------------");
+    Console.ResetColor();
+}
